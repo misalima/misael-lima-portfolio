ed8207f793c40cdc1c74ae4da6437bdde3bc7c95
Misael Lima <misael.alisson14@gmail.com>
2025-06-26 17:08:21 -0300

    feat: Add About section with profile image and skills

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import About from "../components/About";
 
 export default function HomePage() {
     return (
         <>
             <Header />
             <Hero />
+            <About />
         </>
     );
 }
