fd379bfa5ef3ce7c6f762d96720c5356eb2b3a32
Misael Lima <misael.alisson14@gmail.com>
2025-06-28 16:35:40 -0300

    fix .env variable, and fix spacing in header and hero

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,9 +6,9 @@ import Image from 'next/image'
 export default function Header() {
   return (
     <header className="bg-primary-dark relative z-50 py-4 shadow-md transition-colors duration-300">
-        <div className='container mx-auto px-4 sm:px-6 lg:px-36'>
+        <div className='container mx-auto px-4 sm:px-6 lg:px-20'>
             <div className="flex items-center justify-between h-16">
-                <div className="flex items-center justify-center">
+                <div className="flex items-center">
                     <a href="/">
                        <Image src="/misael_logo.png" width={52} height={52} alt='logo'/>
                     </a>
